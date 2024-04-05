0084373b115c10e5cdcd3bbd9c0cff936c1ec9ad
Matt Eddy <matteddy1@gmail.com>
2024-04-05 14:52:30 -0400

    WIP

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>LinkedIn: Log In or Sign Up</title>
+        <meta name="description" content="1 billion members | Manage your professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities." />
+
+        <meta property="og:url" content="https://www.linkedin.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="LinkedIn: Log In or Sign Up" />
+        <meta property="og:description" content="1 billion members | Manage your professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities." />
+        <meta property="og:image" content="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="linkedin.com" />
+        <meta property="twitter:url" content="https://www.linkedin.com/" />
+        <meta name="twitter:title" content="LinkedIn: Log In or Sign Up" />
+        <meta name="twitter:description" content="1 billion members | Manage your professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities." />
+        <meta name="twitter:image" content="/favicon.ico" />
+      </head>
       <body className={`bg-bg text-text ${gothic.className}`}>{children}</body>
     </html>
   );
